632bc231841415ecd13c83b790541f884bf7efe4
jyothi1802 <46337064+jyothi1802@users.noreply.github.com>
2019-01-03 16:26:07 +0530

    Update StringintoWords.py

--- StringintoWords.py
+++ StringintoWords.py
@@ -1,9 +1,13 @@
-#Write a python program to given string split into list of words
+#Write a python program to given string split into list of sentences and words 
 
 def st():
     my_str = input("Enter a string: ")
-     # breakdown the string into a list of words
+    # breakdown the string into a list of words
     words = my_str.split()
+     # breakdown the string into a list of sentences
+    sentence = my_str.split(".")
+    print(sentence)
+    print(len(sentence))
     return words
     for word in words:
         print(word)
